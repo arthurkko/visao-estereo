62d9fb538a89e979a374003835bfc74a0662964d
arthurkko <arthurkko@gmail.com>
2023-05-11 19:00:01 -0300

    implemment match boxes

---
+++ utils/box_matching.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def parse(boxes):
+    detect = np.array([[0,0,0,0,0]])
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+
+        area = box[2]*box[3]
+        cx = box[0]
+        cy = box[1]
+        h = box[3]
+        w = box[2]
+
+        d = np.array([[area, cx, cy, h, w]])
+        detect = np.append(detect, d, axis=0)
+    
+    detect = np.delete(detect, 0, axis=0)
+
+    return detect
+
+def match_box(boxes1, boxes2):
+    pairs = []
+
+    n1 = boxes1.shape[0]
+    n2 = boxes2.shape[0]
+    
+    flag = n1<=n2
+        
+    for i1, b in enumerate(boxes1 if flag else boxes2):
+        dif = (boxes1 if not flag else boxes2) - b
+        sum_abs = np.sum(np.abs(dif), axis=1)
+        i2 = np.argmin(sum_abs)
+        if flag:
+            pairs.append((i1,i2))
+        else:
+            pairs.append((i2,i1))
+
+        if i1==1 and True:
+            print(boxes1)
+            print(boxes1.shape)
+            print(boxes2)
+            print(boxes2.shape)
+            print('boxes1' if flag else 'boxes2')
+            print('b')
+            print(b)
+            print('dif')
+            print(dif)
+            print('sum_abs')
+            print(sum_abs)
+            print(f"box{pairs[i1][0]} of boxes1 is the same of box{pairs[i1][1]} of boxes2")
+
+if __name__=="__main__":
+    print('oi')
--- yolov8.py
+++ yolov8.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 from ultralytics import YOLO
-from rectify_image_5 import rectify_frame
 import numpy as np
+import time
+
+from utils.rectify_image import rectify_frame
+from utils.box_matching import parse, match_box
 
 gravar = 0
 # Load the YOLOv8 model
@@ -9,61 +12,55 @@ model = YOLO('yolov8n.pt')
 
 # Open the video file
 video_path = "video_416_p.avi"
-cap_e = cv.VideoCapture(0)
+cap_e = cv.VideoCapture(2)
 if not cap_e.isOpened():
     print("Cannot open camera")
     exit()
 cap_e.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 cap_e.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
-# cap_d= cv.VideoCapture(4) 
-# if not cap_d.isOpened():
-#     print("Cannot open camera")
-#     exit()
-# cap_d.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-# cap_d.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+cap_d= cv.VideoCapture(4) 
+if not cap_d.isOpened():
+    print("Cannot open camera")
+    exit()
+cap_d.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+cap_d.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
 if gravar:
     fourcc = cv.VideoWriter_fourcc(*'MJPG')
     out = cv.VideoWriter('./note_416_41fps.avi', fourcc, 24.0, (416, 416))
     
+
 # Loop through the video frames
 while cap_e.isOpened():
     # Read a frame from the video
     ret_e, frame_e = cap_e.read()
-    # ret_d, frame_d = cap_d.read()
+    ret_d, frame_d = cap_d.read()
 
     if ret_e:
         # Run YOLOv8 inference on the frame
-        # r_e = rectify_frame(frame_e, 0)
-        # r_d = rectify_frame(frame_d, 1)
+        r_e = rectify_frame(frame_e, 0)
+        r_d = rectify_frame(frame_d, 1)
 
-        results_e = model(frame_e, imgsz=416, conf=0.3)
-        # results_d = model(r_d, imgsz=416, conf=0.3)
-        detect = np.array([[0,0,0,0,0]])
+        results_e = model(r_e, imgsz=416, conf=0.3, verbose=False)
+        results_d = model(r_d, imgsz=416, conf=0.3, verbose=False)
 
-        boxes = results_e[0].boxes.xywh.cpu().numpy()
-        for i in range(len(boxes)):
-            box = boxes[i]
+        res_e = results_e[0].boxes.cpu().numpy()
+        res_d = results_d[0].boxes.cpu().numpy()
 
-            area = box[2]*box[3]
-            cx = box[0]
-            cy = box[1]
-            h = box[3]
-            w = box[2]
-
-            d = np.array([[area, cx, cy, h, w]])
-            detect = np.append(detect, d, axis=0)
+        match_e = parse(res_e.xywh)
+        match_d = parse(res_d.xywh)
         
-        detect = np.delete(detect, 0, axis=0)
-        print('##################################################################################################################')
+        pairs = match_box(match_e, match_d)
+
         # Visualize the results on the frame
         annotated_frame_e = results_e[0].plot()
-        # annotated_frame_d = results_d[0].plot()
+        annotated_frame_d = results_d[0].plot()
 
+    
         # Display the annotated frame
         cv.imshow("YOLOv8 e", annotated_frame_e)
-        # cv.imshow("YOLOv8 d", annotated_frame_d)
+        cv.imshow("YOLOv8 d", annotated_frame_d)
 
         if gravar:
             out.write(annotated_frame_e)
