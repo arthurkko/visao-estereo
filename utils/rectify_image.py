fix: distance calculation. Simplified formula and constant correction addition

utils/rectify_image.py
@@ -1,109 +1,156 @@
 import cv2 as cv 
 import numpy as np
 import matplotlib as plt
-
-##################################################### 5 GRUAS #####################################################
-# cameraMatrix1 = np.array([[812.4393, -0.6777, 668.0206],
-#                           [0, 812.9133, 373.3885],
-#                           [0, 0, 1]])
-# distCoeffs1 = np.array([-0.3875, 0.1561, 4.2380e-04, -8.4097e-04])
-# cameraMatrix2 = np.array([[801.7487, 0.1210, 622.6017],
-#                           [0, 802.4932, 416.6464],
-#                           [0, 0, 1]])
-# distCoeffs2 = np.array([-0.3976, 0.1652, 0.0020, -6.5162e-04])
-imageSize = (1280, 720)
-R = np.array([[1, -8.5426e-04, 0.0086],
-              [8.1895e-04, 1, 0.0041],
-              [-0.0086, -0.0041, 1]])
-T = np.array([-354.0020, -1.1359, 1.7502])
-
-##################################################### 15 GRUAS #####################################################
-cameraMatrix1 = np.array([[828.1032, 0.8006, 676.2680],
-                          [0, 828.9722, 373.6433],
-                          [0, 0, 1]])
-distCoeffs1 = np.array([-0.4025, 0.1630, 5.6058e-04, -6.5175e-04])
-cameraMatrix2 = np.array([[806.7682, 1.1066, 632.5296],
-                          [0, 807.3399, 421.4820],
-                          [0, 0, 1]])
-distCoeffs2 = np.array([-0.4131, 0.1823, 0.0011, -8.2555e-04])
-# imageSize = (1280, 720)
-# R = np.array([[1, -4.8655e-04, 0.0072],
-#               [4.4810e-04, 1, 0.0053],
-#               [-0.0072, -0.0053, 1]])
-# T = np.array([-353.8582, -1.1266, -0.4277])
-
-#perform the rectification
-R1, R2, P1, P2, Q, roi1, roi2 = cv.stereoRectify(cameraMatrix1, distCoeffs1,
-                                                 cameraMatrix2, distCoeffs2,
-                                                 imageSize,
-                                                 R, T, alpha=1.0)
-mapx1, mapy1 = cv.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1,
-                                          imageSize,
-                                          cv.CV_32FC2)
-mapx2, mapy2 = cv.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2,
-                                          imageSize,
-                                          cv.CV_32FC2)
+from numpy.linalg import inv
 
 class CameraParams():
-    def __init__(self, esq=cameraMatrix1, dir=cameraMatrix2) -> None:
-        self.fxe = esq[0][0]
-        self.fye = esq[1][1]
-        self.u0e = esq[0][2]
-        self.v0e = esq[1][2]
+    def __init__(self):
+        # cameraMatrix1 = np.array([[820.0758, 0, 665.1652],
+        #                           [0, 820.1426, 372.9476],
+        #                           [0, 0, 1]])
+        # distCoeffs1 = np.array([-0.3976, 0.1643, -0.0003, -0.0001])
+        # cameraMatrix2 = np.array([[809.2298, 0, 601.2358],
+        #                           [0, 809.6127, 416.0780],
+        #                           [0, 0, 1]])
+        # distCoeffs2 = np.array([-0.4072, 0.1731, 0.0031, -0.0002])
 
-        self.fxd = dir[0][0]
-        self.fyd = dir[1][1]
-        self.u0d = dir[0][2]
-        self.v0d = dir[1][2]
+        # R = np.array([[1, -0.0006, 0.0067],
+        #             [0.0006, 1, 0.0042],
+        #             [-0.0067, -0.0042, 1]])
+        # T = np.array([-354.0233, -1.1110, 1.2614])
 
-def rectify_frame(img, index):
+        ##################################################### 5 GRUAS #####################################################
+        cameraMatrix1 = np.array([[812.4393, -0.6777, 668.0206],
+                                  [0, 812.9133, 373.3885],
+                                  [0, 0, 1]])
+        distCoeffs1 = np.array([-0.3875, 0.1561, 4.2380e-04, -8.4097e-04])
+        cameraMatrix2 = np.array([[801.7487, 0.1210, 622.6017],
+                                  [0, 802.4932, 416.6464],
+                                  [0, 0, 1]])
+        distCoeffs2 = np.array([-0.3976, 0.1652, 0.0020, -6.5162e-04])
+
+        R = np.array([[1, -8.5426e-04, 0.0086],
+                    [8.1895e-04, 1, 0.0041],
+                    [-0.0086, -0.0041, 1]])
+        T = np.array([-354.0020, -1.1359, 1.7502])
+        ##################################################### 15 GRUAS #####################################################
+        # cameraMatrix1 = np.array([[828.1032, 0.8006, 676.2680],
+        #                         [0, 828.9722, 373.6433],
+        #                         [0, 0, 1]])
+        # distCoeffs1 = np.array([-0.4025, 0.1630, 5.6058e-04, -6.5175e-04])
+        # cameraMatrix2 = np.array([[806.7682, 1.1066, 632.5296],
+        #                         [0, 807.3399, 421.4820],
+        #                         [0, 0, 1]])
+        # distCoeffs2 = np.array([-0.4131, 0.1823, 0.0011, -8.2555e-04])
+        # R = np.array([[1, -4.8655e-04, 0.0072],
+        #               [4.4810e-04, 1, 0.0053],
+        #               [-0.0072, -0.0053, 1]])
+        # T = np.array([-353.8582, -1.1266, -0.4277])
+        ####################################################################################################################
+
+        imageSize = (1280, 720)
+
+        #perform the rectification
+        R1, R2, P1, P2, Q, roi1, roi2 = cv.stereoRectify(cameraMatrix1, distCoeffs1,
+                                                        cameraMatrix2, distCoeffs2,
+                                                        imageSize,
+                                                        R, T, alpha=1.0)
+        self.mapx1, self.mapy1 = cv.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1,
+                                                imageSize,
+                                                cv.CV_32FC2)
+        self.mapx2, self.mapy2 = cv.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2,
+                                                imageSize,
+                                                cv.CV_32FC2)
+        
+        self.T = T
+        self.D1 = distCoeffs1
+        self.D2 = distCoeffs2
+
+        self.fxe = cameraMatrix1[0][0]
+        self.fye = cameraMatrix1[1][1]
+        self.u0e = cameraMatrix1[0][2]
+        self.v0e = cameraMatrix1[1][2]
+        self.se = cameraMatrix1[0][1]
+
+        self.fxd = cameraMatrix2[0][0]
+        self.fyd = cameraMatrix2[1][1]
+        self.u0d = cameraMatrix2[0][2]
+        self.v0d = cameraMatrix2[1][2]
+        self.sd = cameraMatrix2[0][1]
+
+        self.dx = T[0]
+        self.dy = T[1]
+
+        self.roi1 = roi1
+        self.roi2 = roi2
+            
+        Me = np.c_[cameraMatrix1, np.zeros((3,1))]
+        Md = np.c_[cameraMatrix2, np.zeros((3,1))]
+
+        H = np.r_[np.c_[np.around(R), T], [np.array([0,0,0,1])]]
+     
+        self.M = np.dot(Me,H)
+       
+def rectify_frame(img, index, mapx1, mapy1, mapx2, mapy2):
     try:
         if index==0:
             img_rect = cv.remap(img, mapx1, mapy1, cv.INTER_LINEAR)
         else:
             img_rect = cv.remap(img, mapx2, mapy2, cv.INTER_LINEAR)
-
-        rect = (roi2[0], roi1[1], roi1[2]+55, roi1[3]+88)
-        img_rect = img_rect[rect[1]:rect[3], rect[0]:rect[2], :]
-
+  
     except:
-        img_rect = np.zeros((498, 1115, 3))
+        img_rect = np.zeros((498, 1092, 3))
 
-    # draw the images side by side
-    # total_size = (max(img_rect1.shape[0], img_rect2.shape[0]),
-    #                 img_rect1.shape[1] + img_rect2.shape[1], 3)
-    # img = np.zeros(total_size, dtype=np.uint8)
-    # img[:img_rect1.shape[0], :img_rect1.shape[1]] = img_rect1
-    # img[:img_rect2.shape[0], img_rect1.shape[1]:] = img_rect2
-
-    # draw horizontal lines every 25 px accross the side by side image
-    # for i in range(20, img.shape[0], 30):
-    #     cv.line(img, (0, i), (img.shape[1], i), (255, 0, 0))
-
-    # cv.rectangle(img_rect1,rect[:2], rect[2:],(0,255,0))
-    # cv.rectangle(img_rect2,rect[:2], rect[2:],(255,0,0))
-
-    # cv.imshow('img', img)
-    
-    # cv.imshow('img2', img_rect2)
     return img_rect
 
+def crop_frame(img, roi1, roi2):
+    rect = (roi2[0], roi1[1], roi1[2]+44, roi2[3]+73)
+    # rect = (roi2[0]+10, roi1[1], roi2[2]+57, roi2[3]+73)
+
+    img_crop = img[rect[1]:rect[3], rect[0]:rect[2], :]
+
+    return img_crop
 
 if __name__ == "__main__":
     p = CameraParams()
-    print(p.fxe)
 
-    cap = cv.VideoCapture("/home/smir/Desktop/Visao Estereo/video/Esquerda/video04_29_13_06.avi")
+
+    cap1 = cv.VideoCapture('/home/smir/Desktop/Visao Estereo/video/Esquerda/video05_15_18_29.avi')
+    cap2 = cv.VideoCapture('/home/smir/Desktop/Visao Estereo/video/Direita/video05_15_18_29.avi')
     while 1:
-        _, f = cap.read()
-        r = rectify_frame(f)
+        _, f1 = cap1.read()
+        _, f2 = cap2.read()
+        img_rect1 = rectify_frame(f1, 0, p.mapx1, p.mapy1, p.mapx2, p.mapy2)
+        img_rect2 = rectify_frame(f2, 1, p.mapx1, p.mapy1, p.mapx2, p.mapy2)
+        
+        # img_rect1 = crop_frame(img_rect1, p.roi1, p.roi2)
+        # img_rect2 = crop_frame(img_rect2, p.roi1, p.roi2)
+        # print(img_rect2.shape)
 
-        cv.imshow("frame", f)
-        cv.imshow("rect", r)
+        # draw the images side by side
+        total_size = (max(img_rect1.shape[0], img_rect2.shape[0]),
+                        img_rect1.shape[1] + img_rect2.shape[1], 3)
+        img = np.zeros(total_size, dtype=np.uint8)
+        img[:img_rect1.shape[0], :img_rect1.shape[1]] = img_rect1
+        img[:img_rect2.shape[0], img_rect1.shape[1]:] = img_rect2
 
-        if r.shape!=(498, 1115, 3):
-            print(f.shape)
-            print(r.shape)
+        # draw horizontal lines every 25 px accross the side by side image
+        for i in range(20, img.shape[0], 30):
+            cv.line(img, (0, i), (img.shape[1], i), (255, 0, 0))
+
+        print('roi1: ', p.roi1)
+        print('roi2: ', p.roi2)
+        print('')
+
+        rect1 = (p.roi1[0], p.roi1[1], p.roi1[2]+44, p.roi2[3]+73)
+        rect2 = (p.roi2[0], p.roi1[1], p.roi2[2]+83, p.roi2[3]+73)
+        
+        cv.rectangle(img_rect1,rect1[:2], rect1[2:],(0,255,0))
+        cv.rectangle(img_rect2,rect1[:2], rect1[2:],(255,0,0))
+
+        cv.imshow('img', img)
+        cv.imshow('img1', img_rect1)
+        cv.imshow('img2', img_rect2)
 
         if cv.waitKey(1)==ord('q'):
             break

yolov8.py
@@ -2,25 +2,27 @@ import cv2 as cv
 from ultralytics import YOLO
 import numpy as np
 import time
-import random
 
-from utils.rectify_image import rectify_frame, CameraParams
+from utils.rectify_image import rectify_frame, crop_frame, CameraParams
 from utils.box_matching import parse, match_box
-
+from utils.world_to_camera import calculate_dist, calculate_disp
 gravar = 0
+
 # Load the YOLOv8 model
 model = YOLO('yolov8n.pt')
 
 # Open the video file
-video_path = "video_416_p.avi"
-cap_e = cv.VideoCapture(0)
+
+# cap_e = cv.VideoCapture(4)
+cap_e = cv.VideoCapture('/home/smir/Desktop/Visao Estereo/video/Esquerda/video05_15_18_29.avi')
 if not cap_e.isOpened():
     print("Cannot open camera")
     exit()
 cap_e.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 cap_e.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
-cap_d= cv.VideoCapture(4) 
+# cap_d= cv.VideoCapture(2) 
+cap_d= cv.VideoCapture('/home/smir/Desktop/Visao Estereo/video/Direita/video05_15_18_29.avi') 
 if not cap_d.isOpened():
     print("Cannot open camera")
     exit()
@@ -29,58 +31,120 @@ cap_d.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
 if gravar:
     fourcc = cv.VideoWriter_fourcc(*'MJPG')
-    out = cv.VideoWriter('./note_416_41fps.avi', fourcc, 24.0, (416, 416))
+    out = cv.VideoWriter('./note_416_41fps.avi', fourcc, 24.0, (320, 320))
     
 params = CameraParams()
 
+t1 = time.time() 
+t2 = time.time()
 # Loop through the video frames
 while cap_e.isOpened():
+    # print('fps: ', 1/(t1-t2))
+    t2 = t1
+    t1 = time.time()
+
     # Read a frame from the video
     ret_e, frame_e = cap_e.read()
     ret_d, frame_d = cap_d.read()
 
     if ret_e:
         # Run YOLOv8 inference on the frame
-        r_e = rectify_frame(frame_e, 0)
-        r_d = rectify_frame(frame_d, 1)
-
-        results_e = model(r_e, imgsz=416, conf=0.3, verbose=False)
-        results_d = model(r_d, imgsz=416, conf=0.3, verbose=False)
+        r_e = rectify_frame(frame_e, 0, params.mapx1, params.mapy1, params.mapx2, params.mapy2)
+        r_d = rectify_frame(frame_d, 1, params.mapx1, params.mapy1, params.mapx2, params.mapy2)
+        r_e = crop_frame(r_e, params.roi1, params.roi2)
+        r_d = crop_frame(r_d, params.roi1, params.roi2)
+        # r_e = frame_e
+        # r_d = frame_d
+        
+        # Detecção de objetos
+        results_e = model(r_e, imgsz=320, conf=0.35, verbose=False)
+        results_d = model(r_d, imgsz=320, conf=0.35, verbose=False)
 
         res_e = results_e[0].boxes.cpu().numpy()
         res_d = results_d[0].boxes.cpu().numpy()
 
+        # (x_centro, y_centro, largura, altura) de cada objeto
         match_e = parse(res_e.xywhn)
         match_d = parse(res_d.xywhn)
         
         pairs = match_box(match_e, match_d)
 
         # Visualize the results on the frame
-        annotated_frame_e = results_e[0].plot()
-        annotated_frame_d = results_d[0].plot()
+        annotated_frame_e = results_e[0].plot(labels=False)
+        annotated_frame_d = results_d[0].plot(labels=False)
+ 
+        # Escolhe um objeto de referência
+        # i = 0
+        # p = pairs[i]
 
-        i = 0
-        p = pairs[i]
-        cv.circle(annotated_frame_e,(int(res_e.xywh[p[0]][0]), int(res_e.xywh[p[0]][1])), 15, (0,0,255), 20)
-        cv.circle(annotated_frame_d,(int(res_d.xywh[p[1]][0]), int(res_d.xywh[p[1]][1])), 15, (0,0,255), 20)
-        cv.putText(annotated_frame_e,'dist'+str(i), (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)
-        cv.putText(annotated_frame_d,'dist'+str(i), (int(res_d.xyxy[p[1]][0]+5), int(res_d.xyxy[p[1]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)
+        # cv.circle(annotated_frame_e,(int(res_e.xywh[p[0]][0]), int(res_e.xywh[p[0]][1])), 10, (0,0,255), 15)
+        # cv.circle(annotated_frame_d,(int(res_d.xywh[p[1]][0]), int(res_d.xywh[p[1]][1])), 10, (0,0,255), 15)
+        # cv.putText(annotated_frame_e,'dist'+str(i), (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)
+        # cv.putText(annotated_frame_d,'dist'+str(i), (int(res_d.xyxy[p[1]][0]+5), int(res_d.xyxy[p[1]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)
        
-        # for i, p in enumerate(pairs):
-        #     cv.putText(annotated_frame_e,'dist'+str(i), (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)
-        #     cv.putText(annotated_frame_d,'dist'+str(i), (int(res_d.xyxy[p[1]][0]+5), int(res_d.xyxy[p[1]][3]-5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 5)    
+        
+        for p in pairs:
+            dist = calculate_dist(res_e.xywh, res_d.xywh, p, params)
+            # disp = calculate_disp(res_e.xywh, res_d.xywh, p, params)
+            disp = int(res_e.xywh[p[0]][0] - res_d.xywh[p[1]][0])
+            
+            # cv.circle(annotated_frame_e,(int(res_e.xywh[p[0]][0]), int(res_e.xywh[p[0]][1])), 10, (0,0,255), 15)
+            # cv.circle(annotated_frame_d,(int(res_d.xywh[p[1]][0]), int(res_d.xywh[p[1]][1])), 10, (0,0,255), 15)
+            cv.putText(annotated_frame_e, f'px:{int(res_e.xywh[p[0]][0])}',
+                       (int(res_e.xyxy[p[0]][2]-60), int(res_e.xyxy[p[0]][1]-25)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_e, f'py:{int(res_e.xywh[p[0]][1])}',
+                       (int(res_e.xyxy[p[0]][2]-60), int(res_e.xyxy[p[0]][1]-10)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_d, f'px:{int(res_d.xywh[p[1]][0])}',
+                       (int(res_d.xyxy[p[1]][2]-60), int(res_d.xyxy[p[1]][1]-25)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_d, f'py:{int(res_d.xywh[p[1]][1])}',
+                       (int(res_d.xyxy[p[1]][2]-60), int(res_d.xyxy[p[1]][1]-10)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            
+            cv.putText(annotated_frame_e, f'x:{str(dist[0])}',
+                       (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-35)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_e, f'y:{str(dist[1])}',
+                       (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-20)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_e, f'z:{str(dist[2])}',
+                       (int(res_e.xyxy[p[0]][0]+5), int(res_e.xyxy[p[0]][3]-5)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+            cv.putText(annotated_frame_d, f'disp:{disp}',
+                       (int(res_d.xyxy[p[1]][0]+5), int(res_d.xyxy[p[1]][3]-5)),
+                       cv.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+        
+
+        # Markng important points 
+        # cv.circle(frame_e, (int(params.u0e), int(params.v0e)), 5, (0,255,0), 10) # (cx, cy) left camera
+        # cv.circle(frame_d, (int(params.u0d), int(params.v0d)), 5, (0,255,0), 10) # (cx, cy) right camera
+        # cv.circle(annotated_frame_e, (int(params.u0e), int(params.v0e)), 5, (0,255,0), 10) # (cx, cy) left camera
+        # cv.circle(annotated_frame_d, (int(params.u0d), int(params.v0d)), 5, (0,255,0), 10) # (cx, cy) right camera
+        # cv.circle(annotated_frame_e, (546,242), 5, (0,255,0), 10) # (cx, cy) left camera
+        # cv.circle(annotated_frame_d, (546,242), 5, (0,255,0), 10) # (cx, cy) right camera
+        
+        # Crop rectified images
+        # annotated_frame_e = rectify_frame(annotated_frame_e, 0, params.mapx1, params.mapy1, params.mapx2, params.mapy2)
+        # annotated_frame_d = rectify_frame(annotated_frame_d, 1, params.mapx1, params.mapy1, params.mapx2, params.mapy2)
+        # annotated_frame_e = crop_frame(annotated_frame_e, params.roi1, params.roi2)
+        # annotated_frame_d = crop_frame(annotated_frame_d, params.roi1, params.roi2)
+
         # Display the annotated frame
         s = annotated_frame_e.shape
-        crop_e = annotated_frame_e[100:s[0]-100, :, :]
-        crop_d = annotated_frame_d[100:s[0]-100, :, :]
+        crop_e = annotated_frame_e[150:s[0], :, :]
+        crop_d = annotated_frame_d[150:s[0], :, :]
         sc = crop_e.shape
         im = np.zeros((sc[0]*2, sc[1], sc[2]),np.uint8)
         im[:sc[0],:,:] = crop_e
         im[sc[0]:,:,:] = crop_d
+        # cv.imshow("Original e", frame_e)
+        # cv.imshow("Original d", frame_d)
         cv.imshow("YOLOv8 e", annotated_frame_e)
         cv.imshow("YOLOv8 d", annotated_frame_d)
         cv.imshow('concat', im)
-
+        
         if gravar:
             out.write(annotated_frame_e)
             out.write(annotated_frame_d)
@@ -88,13 +152,17 @@ while cap_e.isOpened():
         # Break the loop if 'q' is pressed
         if cv.waitKey(1) & 0xFF == ord("q"):
             break
+        else:
+            cv.imwrite("./ime.png", annotated_frame_e)
+            cv.imwrite("./imd.png", annotated_frame_d)
+
+        
     else:
         # Break the loop if the end of the video is reached
         break
 
 # Release the video capture object and close the display window
 cap_e.release()
-# cap_d.release()
-if gravar:
-    out.release()
+cap_d.release()
+
 cv.destroyAllWindows()